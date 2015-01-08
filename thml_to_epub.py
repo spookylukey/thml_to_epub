f77d0bd42f85d57f180d0ef7ae2ecbaad7392370
Luke Plant <L.Plant.98@cantab.net>
2015-01-08 14:22:55 +0000

    Refactored into 'main' function

--- thml_to_epub.py
+++ thml_to_epub.py
@@ -2,61 +2,59 @@
 
 import os.path
 import zipfile
-
 import sys
 
-inputfile = sys.argv[1]
 
-print "Converting {0}".format(inputfile)
+def main(inputfiles):
+    outputfile = inputfiles[0].replace('.xml', '').replace('.thml', '') + ".rough.epub"
 
-thml = file(inputfile).read()
+    print "Creating {0}".format(outputfile)
+    epub = zipfile.ZipFile(outputfile, "w", zipfile.ZIP_DEFLATED)
 
-outputfile = inputfile.replace('.xml', '').replace('.thml', '') + ".rough.epub"
+    epub.writestr("mimetype", "application/epub+zip", zipfile.ZIP_STORED)
 
-epub = zipfile.ZipFile(outputfile, "w", zipfile.ZIP_DEFLATED)
 
-epub.writestr("mimetype", "application/epub+zip", zipfile.ZIP_STORED)
+    # We need an index file, that lists all other HTML files
+    # This index file itself is referenced in the META_INF/container.xml
+    # file
+    epub.writestr("META-INF/container.xml", '''<container version="1.0"
+               xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+      <rootfiles>
+        <rootfile full-path="OEBPS/Content.opf" media-type="application/oebps-package+xml"/>
+      </rootfiles>
+    </container>''', zipfile.ZIP_STORED);
 
-# The filenames of the HTML are listed in html_files
-html_files = [inputfile]
+    # The index file is another XML file, living per convention
+    # in OEBPS/Content.xml
+    index_tpl = '''<package version="2.0"
+      xmlns="http://www.idpf.org/2007/opf">
+      <metadata/>
+      <manifest>
+        %(manifest)s
+      </manifest>
+      <spine toc="ncx">
+        %(spine)s
+      </spine>
+    </package>'''
 
-# We need an index file, that lists all other HTML files
-# This index file itself is referenced in the META_INF/container.xml
-# file
-epub.writestr("META-INF/container.xml", '''<container version="1.0"
-           xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
-  <rootfiles>
-    <rootfile full-path="OEBPS/Content.opf" media-type="application/oebps-package+xml"/>
-  </rootfiles>
-</container>''', zipfile.ZIP_STORED);
+    manifest = ""
+    spine = ""
 
-# The index file is another XML file, living per convention
-# in OEBPS/Content.xml
-index_tpl = '''<package version="2.0"
-  xmlns="http://www.idpf.org/2007/opf">
-  <metadata/>
-  <manifest>
-    %(manifest)s
-  </manifest>
-  <spine toc="ncx">
-    %(spine)s
-  </spine>
-</package>'''
+    # Write each HTML file to the ebook, collect information for the index
+    for i, html_file in enumerate(inputfiles):
+        basename = os.path.basename(html_file)
+        manifest += '<item id="file_%s" href="%s" media-type="application/xhtml+xml"/>' % (
+            i+1, basename)
+        spine += '<itemref idref="file_%s" />' % (i+1)
+        epub.write(html_file, 'OEBPS/'+basename, zipfile.ZIP_DEFLATED)
 
-manifest = ""
-spine = ""
+    # Finally, write the index
+    epub.writestr('OEBPS/Content.opf', index_tpl % {
+      'manifest': manifest,
+      'spine': spine,
+    })
+    epub.close()
 
-# Write each HTML file to the ebook, collect information for the index
-for i, html_file in enumerate(html_files):
-    basename = os.path.basename(inputfile)
-    manifest += '<item id="file_%s" href="%s" media-type="application/xhtml+xml"/>' % (
-        i+1, basename)
-    spine += '<itemref idref="file_%s" />' % (i+1)
-    epub.write(html_file, 'OEBPS/'+basename, zipfile.ZIP_DEFLATED)
 
-# Finally, write the index
-epub.writestr('OEBPS/Content.opf', index_tpl % {
-  'manifest': manifest,
-  'spine': spine,
-})
-epub.close()
+if __name__ == '__main__':
+    main(sys.argv[1:])
